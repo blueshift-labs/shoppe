ffbdd9006c5e820e026d63ed2ac233eb950676cc
apsc92 <anurag@joshsoftware.com>
2016-08-26 22:09:27 +0530

    Updated reset password text

--- app/mailers/shoppe/customer_mailer.rb
+++ app/mailers/shoppe/customer_mailer.rb
@@ -2,7 +2,7 @@ module Shoppe
   class CustomerMailer < ActionMailer::Base
     def new_password(customer)
       @customer = customer
-      mail from: Shoppe.settings.outbound_email_address, to: customer.email, subject: 'Your new Shoppe password'
+      mail from: Shoppe.settings.outbound_email_address, to: customer.email, subject: 'Your new BlueshiftReads password'
     end
   end
 end
